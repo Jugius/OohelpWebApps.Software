db2fcb06fdeef6b6f8f0243dfa6abbf4c91055eb
Andrew Slusarev <jugius@gmail.com>
2024-02-22 15:16:53 +0200

    Load applications dataset on startup

--- OohelpWebApps.Software.Client.SoftwareManager/App.xaml.cs
+++ OohelpWebApps.Software.Client.SoftwareManager/App.xaml.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace SoftwareManager
+namespace SoftwareManager;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    protected override void OnStartup(StartupEventArgs e)
     {
+        var mainWindow = new MainWindow();
+        this.MainWindow = mainWindow;
+        this.MainWindow.Show();
+        mainWindow.ReloadDataset.Execute(null);
+        base.OnStartup(e);
     }
 }
